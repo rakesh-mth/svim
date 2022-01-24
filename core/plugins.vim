fa387d79b5ac6586a561ab32c3c8ec95a039f4ab
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2022-01-23 22:53:42 -0500

    install treesitter textobjects, refactor and playground

--- core/plugins.vim
+++ core/plugins.vim
@@ -52,10 +52,14 @@ call plug#begin(g:UC_PLUGGED_DIR)
     Plug 'godlygeek/tabular' " Tabular for aligning texts in tabular format
     Plug 'phaazon/hop.nvim', Cond(has('nvim')) " easy motion like plugin
     if has('nvim-0.5')
+        Plug 'nvim-lua/plenary.nvim'  " dependencies for null-ls
         Plug 'kyazdani42/nvim-web-devicons' " for file icons in nvim-tree. needed by nvim-tree.
         Plug 'kyazdani42/nvim-tree.lua' " A File Explorer For Neovim Written In Lua
+        " treesitter plugins
+        Plug 'nvim-treesitter/playground'
+        Plug 'nvim-treesitter/nvim-treesitter-refactor'
+        Plug 'nvim-treesitter/nvim-treesitter-textobjects'
         Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'} " treesitter for better text color
-        Plug 'nvim-lua/plenary.nvim'  " dependencies for null-ls
         " completion plugins
         Plug 'hrsh7th/cmp-nvim-lsp' " add nvim_lsp source in nvim-cmp. completion using lsp.
         Plug 'hrsh7th/cmp-nvim-lua' " add nvim_lua source in nvim-cmp.
@@ -65,12 +69,12 @@ call plug#begin(g:UC_PLUGGED_DIR)
         Plug 'hrsh7th/cmp-cmdline'
         Plug 'saadparwaiz1/cmp_luasnip' " snippet completions
         Plug 'hrsh7th/nvim-cmp'         " main comletion engine
+        " completion plugins - not used currently
+        Plug 'ms-jpq/coq_nvim', {'branch': 'coq'} " run :COQdeps (only once) and then :COQnow
+        Plug 'ms-jpq/coq.artifacts', {'branch': 'artifacts'}
         " snippets plugins
         Plug 'L3MON4D3/LuaSnip' " snippet engine
         Plug 'rafamadriz/friendly-snippets' " a bunch of snippets to use
-        " completion plugin not used currently
-        Plug 'ms-jpq/coq_nvim', {'branch': 'coq'} " run :COQdeps (only once) and then :COQnow
-        Plug 'ms-jpq/coq.artifacts', {'branch': 'artifacts'}
         " lsp plugins
         Plug 'neovim/nvim-lspconfig'  " nvim lsp configure
         Plug 'williamboman/nvim-lsp-installer' " nvim lsp install and configure using nvim-lspconfig
--- lua/svim/treesitter.lua
+++ lua/svim/treesitter.lua
@@ -3,32 +3,153 @@ if not status_ok then
 	return
 end
 
-status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
+local treesitter_config_status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
+if not treesitter_config_status_ok then
 	return
 end
 
-treesitter_configs.setup({
-	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-	ensure_installed = "maintained",
+local treesitter_config = {
+    -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = "maintained",
 
-	-- Install languages synchronously (only applied to `ensure_installed`)
-	sync_install = false,
+    -- Install languages synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-	-- List of parsers to ignore installing
-	ignore_install = { "haskell" },
+    -- List of parsers to ignore installing
+    ignore_install = { "haskell" },
 
-	highlight = {
-		-- `false` will disable the whole extension
-		enable = true,
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
 
-		-- list of language that will be disabled
-		-- disable = { "c", "rust" },
+        -- list of language that will be disabled
+        -- disable = { "c", "rust" },
 
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
-})
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true,
+        disable = { "yaml" },
+    },
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+    },
+}
+
+-- treesitter playground
+local treesitter_playground_status_ok, _ = pcall(require, "nvim-treesitter-playground")
+if treesitter_playground_status_ok then
+    treesitter_config["playground"] = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+        },
+    }
+end
+
+
+-- treesitter textobjects
+local treesitter_textobjects_status_ok, _ = pcall(require, "nvim-treesitter-textobjects")
+if treesitter_textobjects_status_ok then
+    -- error "found treesitter textobjects"
+    treesitter_config["textobjects"] = {
+        select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = { -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>ta"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>tA"] = "@parameter.inner",
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer",
+            },
+        },
+        lsp_interop = {
+            enable = true,
+            border = 'none',
+            peek_definition_code = {
+                ["<leader>df"] = "@function.outer",
+                ["<leader>dF"] = "@class.outer",
+            },
+        },
+    }
+end
+
+
+-- treesitter refactor
+local treesitter_refactor_status_ok, _ = pcall(require, "nvim-treesitter-refactor")
+if treesitter_refactor_status_ok then
+    treesitter_config["refactor"] = {
+        highlight_definitions = {
+            enable = true,
+            clear_on_cursor_move = true, -- Set to false if you have an `updatetime` of ~100.
+        },
+        highlight_current_scope = {
+            enable = false,
+        },
+        smart_rename = {
+            enable = true,
+            keymaps = {
+                smart_rename = "grr", -- TODO: change this mapping
+            },
+        },
+        navigation = {
+            enable = false, -- TODO: change mapping and enable this
+            keymaps = {
+                goto_definition = "gnd",
+                list_definitions = "gnD",
+                list_definitions_toc = "gO",
+                goto_next_usage = "<a-*>",
+                goto_previous_usage = "<a-#>",
+            },
+        },
+    }
+end
+
+treesitter_configs.setup(treesitter_config)
+
